619e610178e02d7d023b7917a55c1f956a07a165
unknown <emonhossen1000@gmail.com>
2023-11-03 21:20:18 +0600

    schedual added

---
+++ src/Components/Schedule.jsx
@@ -0,0 +1,51 @@
+import { BsAlarm } from "react-icons/bs";
+import { AiOutlineCar } from "react-icons/ai";
+import { FiPhoneCall } from "react-icons/fi";
+
+import "./schedule.css";
+
+const Schedule = () => {
+  return (
+    <div className="w-9/12 mx-auto color  my-10">
+      <section className="flex container justify-between">
+        <div className="flex items-center gap-3">
+          <div>
+            <BsAlarm className="text-3xl text-sky-300  "></BsAlarm>
+          </div>
+          <div className="flex flex-col">
+            <p>MON - FRI / 8:00 - 18:00</p>
+            <p>Working Days/Hours!</p>
+          </div>
+        </div>
+        {/* 2nd */}
+        <div className="flex items-center gap-3">
+          <div>
+            <AiOutlineCar className="text-3xl text-sky-300  "></AiOutlineCar>
+          </div>
+          <div className="flex flex-col">
+            <p>MON - FRI / 8:00 - 18:00</p>
+            <p>Working Days/Hours!</p>
+          </div>
+        </div>
+        {/* 3rd */}
+        <div className="flex items-center gap-3">
+          <div>
+            <FiPhoneCall className="text-3xl text-sky-300  "></FiPhoneCall>
+          </div>
+          <div className="flex flex-col">
+            <p>MON - FRI / 8:00 - 18:00</p>
+            <p>Working Days/Hours!</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Schedule;
+
+{
+  /* 
+                
+                <FiPhoneCall></FiPhoneCall> */
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../Components/Banner";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import Schedule from "../Components/Schedule";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Schedule></Schedule>
 
       <Footer></Footer>
       
